5edaacb4de830c592bbf31754946e6d07e65880f
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-07-12 21:05:58 +0330

    feat: background color added to body; board is made its simple

--- dozz-reactjs-ts/src/components/Board.tsx
+++ dozz-reactjs-ts/src/components/Board.tsx
@@ -2,7 +2,41 @@ import { Component } from "react";
 
 class Board extends Component {
   render() {
-    return <p>Board</p>;
+    return (
+      <table className="border-collapse m-auto">
+        <tr>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-t-0 border-l-0">
+            1
+          </td>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-t-0">
+            1
+          </td>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-t-0 border-r-0">
+            1
+          </td>
+        </tr>
+        <tr>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-l-0">
+            1
+          </td>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black">1</td>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-r-0">
+            1
+          </td>
+        </tr>
+        <tr>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-b-0 border-l-0">
+            1
+          </td>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-b-0">
+            1
+          </td>
+          <td className="w-24 h-24 md:w-48 md:h-48 border-2 border-black border-b-0 border-r-0">
+            1
+          </td>
+        </tr>
+      </table>
+    );
   }
 }
 
