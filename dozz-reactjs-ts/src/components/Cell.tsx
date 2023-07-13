1e54501b1fd31c76242f1575a123dc888c0934a7
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-07-13 19:35:20 +0330

    feat: react icons package added; cirle and cross icons with their styles added; white color change to gray-100 in all components; cell component added

--- dozz-reactjs-ts/src/components/Cell.tsx
+++ dozz-reactjs-ts/src/components/Cell.tsx
@@ -1,8 +1,28 @@
 import { Component } from "react";
+import Cross from "./Cross";
+import Circle from "./Circle";
 
-class Cell extends Component {
+type props = {
+  className?: string;
+  statue: string;
+};
+
+class Cell extends Component<props> {
   render() {
-    return <p>Cell</p>;
+    return (
+      <td
+        className={`w-24 h-24 md:w-48 md:h-48 border-4 border-black transition duration-300 ease-in-out delay-50  
+        ${this.props.className}
+        ${
+          this.props.statue === "empty"
+            ? "hover:bg-gray-100 animate-pulse cursor-pointer"
+            : "hover:text-gray-100"
+        }`}
+      >
+        {this.props.statue === "cross" ? <Cross /> : ""}
+        {this.props.statue === "circle" ? <Circle /> : ""}
+      </td>
+    );
   }
 }
 
